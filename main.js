05bc825cfd17db52a4a24ce5baf4a599827d5697
OlesyaVyskretseva <145101507+OlesyaVyskretseva@users.noreply.github.com>
2024-01-25 11:34:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
+
 document.querySelector("#my-modal .modal__box").addEventListener("click", event => {
   event._isClickWithInModal = true;
 });
@@ -34,4 +35,53 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
 
 document.getElementById('myForm1').addEventListener('submit', function(event) {
   event.preventDefault();
+});
+
+/*jshint esversion: 6 */
+
+const menu = document.querySelector(".header__nav");
+const menuBtn = document.querySelector(".header__icon");
+
+const body = document.body;
+
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+    body.classList.toggle('lock');
+  });
+
+
+menu.querySelectorAll('.header__nav_link').forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active');
+    menuBtn.classList.remove('active');
+    body.classList.remove('lock');
+  });
+});
+}
+
+type="text/javascript">
+    $(document).ready(function(){
+$('.single__item').slick({
+  infinite: true,
+  dots: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+      }
+    }
+  ]
+});
 });
